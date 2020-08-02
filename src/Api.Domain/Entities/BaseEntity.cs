28d3e50e415d5112374f7a2882793ef81f682513
VanderNunesBR <tec.vander.nunes@gmail.com>
2020-08-02 18:03:44 -0300

    Implementado Entidade base e Cliente

---
+++ src/Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        private DateTime? _createAt;
+        public DateTime? CreateAt
+        {
+            get { return _createAt; }
+            set { _createAt = (value != null ? value : DateTime.UtcNow); }
+        }
+
+        public DateTime UpdateAt { get; set; }
+    }
+
+    public class Datetime
+    {
+    }
+}
---
+++ src/Api.Domain/Entities/UserEntity.cs
@@ -0,0 +1,9 @@
+namespace Api.Domain.Entities
+{
+    public class UserEntity : BaseEntity
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+    }
+}
