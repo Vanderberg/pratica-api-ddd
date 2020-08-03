0010932565e49059f6befde44e3376d8fd7634b6
VanderNunesBR <tec.vander.nunes@gmail.com>
2020-08-03 15:04:57 -0300

    Ajuste no IRepository.cs

--- src/Api.Domain/Interfaces/IRepository.cs
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -7,8 +7,8 @@ namespace Api.Domain.Interfaces
 {
     public interface IRepository<T> where T : BaseEntity
     {
-        Task<T> InsertAsync(T Item);
-        Task<T> UpdateAsync(T Item);
+        Task<T> InsertAsync(T item);
+        Task<T> UpdateAsync(T item);
         Task<bool> DeleteAsync(Guid id);
         Task<T> SelectAsync(Guid id);
         Task<IEnumerable<T>> SelectAsync();
