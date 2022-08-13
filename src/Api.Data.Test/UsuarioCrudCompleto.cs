7dd1a765c1e76e81aeb2933f097ba1e36ecbb718
Vanderberg Nunes <tec.vander.nunes@gmail.com>
2022-08-13 15:44:23 -0300

    Criado Testes Unitários

---
+++ src/Api.Data.Test/UsuarioCrudCompleto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Data.Implementations;
+using Api.Domain.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Api.Data.Test
+{
+    public class UsuarioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvider;
+
+        public UsuarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvider = dbTeste.ServiceProvider;
+        }
+        
+        [Fact(DisplayName = "CRUD de Usuário")]
+        [Trait("CRUD", "UserEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Usuario()
+        {
+            using (var context = _serviceProvider.GetService<MyContext>())
+            {
+                UserImplementation _reposotorio = new UserImplementation(context);
+                UserEntity _entity = new UserEntity
+                {
+                    Email = Faker.Internet.Email(),
+                    Nome = Faker.Name.FullName()
+                };
+
+                var _registroCriado = await _reposotorio.InsertAsync(_entity);
+                Assert.NotNull(_registroCriado);
+                Assert.Equal(_entity.Email, _registroCriado.Email);
+                Assert.Equal(_entity.Nome, _registroCriado.Nome);
+                Assert.False(_registroCriado.Id == Guid.Empty);
+
+
+                _entity.Nome = Faker.Name.First();
+                var _RegistroAtualizado = await _reposotorio.UpdateAsync(_entity);
+                Assert.NotNull(_RegistroAtualizado);
+                Assert.Equal(_entity.Email, _RegistroAtualizado.Email);
+                Assert.Equal(_entity.Nome, _RegistroAtualizado.Nome);
+
+                var _registroExiste = await _reposotorio.ExistAsync(_entity.Id);
+
+                var _registroSelecionado = await _reposotorio.SelectAsync(_entity.Id);
+                Assert.NotNull(_registroSelecionado);
+                Assert.Equal(_RegistroAtualizado.Email, _registroSelecionado.Email);
+                Assert.Equal(_RegistroAtualizado.Nome, _registroSelecionado.Nome);
+
+                var _todosRegistros = await _reposotorio.SelectAsync();
+                Assert.NotNull(_todosRegistros);
+                Assert.True(_todosRegistros.Count() > 0);
+            }
+        }
+    }
+}
---
+++ src/Api.Service.Test/Usuario/UsuarioTestes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using Api.Domain.Dtos.User;
+
+namespace Api.Service.Test.Usuario
+{
+    public class UsuarioTestes
+    {
+        public static string NomeUsuario { get; set; }
+        public static string EmailUsuario { get; set; }
+        public static string NomeUsuarioAlterado { get; set; }
+        public static string EmailUsuarioAlterado { get; set; }
+        public static Guid IdUsuario { get; set; }
+
+        public List<UserDto> ListaUserDto = new List<UserDto>();
+        public UserDto UserDto;
+        public UserDtoCreate userDtoCreate;
+        public UserDtoCreateResult userDtoCreateResult;
+        public UserDtoUpdate userDtoUpdate;
+        public UserDtoUpdateResult userDtoUpdateResult;
+
+        public UsuarioTestes()
+        {
+            IdUsuario = Guid.NewGuid();
+            NomeUsuario = Faker.Name.FullName();
+            EmailUsuario = Faker.Internet.Email();
+            NomeUsuarioAlterado = Faker.Name.FullName();
+            EmailUsuarioAlterado = Faker.Internet.Email();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var dto = new UserDto()
+                {
+                    Id = new Guid(),
+                    Nome = Faker.Name.FullName(),
+                    email = Faker.Internet.Email()
+                };
+                ListaUserDto.Add(dto);
+            }
+
+            UserDto = new UserDto()
+            {
+                Id = IdUsuario,
+                Nome = NomeUsuario,
+                email = EmailUsuario
+            };
+
+            userDtoCreate = new UserDtoCreate()
+            {
+                Nome = NomeUsuario,
+                email = EmailUsuario
+            };
+            
+            userDtoCreateResult = new UserDtoCreateResult()
+            {
+                Id = IdUsuario,
+                Nome = NomeUsuario,
+                email = EmailUsuario,
+                CreateAt = DateTime.UtcNow 
+            };
+            
+            userDtoUpdate = new UserDtoUpdate()
+            {
+                Id = IdUsuario,
+                Nome = NomeUsuario,
+                email = EmailUsuario
+            };
+            
+                        
+            userDtoUpdateResult = new UserDtoUpdateResult()
+            {
+                Id = IdUsuario,
+                Nome = NomeUsuario,
+                email = EmailUsuario,
+                UpdateAt = DateTime.UtcNow
+            };
+        }
+    }
+}
